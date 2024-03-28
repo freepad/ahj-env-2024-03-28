2cf65d86691fc7d0118a1357fed595c998302c0a
Damir Rysaev <freepad@ya.ru>
2024-03-28 20:02:07 +0300

    upd code

--- src/js/app.js
+++ src/js/app.js
@@ -9,27 +9,3 @@ export default function demo(value) {
 }
 
 console.log("app.js included");
-
-// ?.
-const user = {
-  id: 10,
-  courses: [{ id: 1, title: 'AHJ' }]
-}
-
-const admin = {
-  id: 1,
-  courses: []
-}
-
-console.log(user.courses[0].title)
-
-console.log(admin.courses && admin.courses.length && admin.courses[0].title)
-
-const admin1 = undefined
-console.log(admin1?.courses[0]?.title) // undefined
-
-const str = ''
-
-if (str.startsWith('http://')) {
-  console.log('test')
-}
